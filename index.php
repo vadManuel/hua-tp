cda11d9a84a0f5c99c21ae736543efaf0cdd319f
Manuel Vasquez <vadmanuel@gmail.com>
2020-04-19 15:56:29 -0400

    add to cart & swapped login and store

--- index.php
+++ index.php
@@ -1,6 +1,46 @@
 <?php
 
+include './utility/util.php';
+
 session_start();
+$con = open_connection();
+setlocale(LC_MONETARY, 'en_US');
+
+$stmt = $con->prepare('SELECT ppt.product_id, ppt.product_name, ppt.image_path, ppt.price, ppt.stock, t.tag_name FROM (SELECT p.product_id as product_id, p.product_name as product_name, p.image_path as image_path, p.price as price, p.stock as stock, pt.tag_id as tag_id FROM products p INNER JOIN product_tags pt ON p.product_tags_id = pt.product_tags_id) ppt INNER JOIN tags t ON ppt.tag_id = t.tag_id');
+$stmt->execute();
+$result = $stmt->get_result();
+
+$products = array();
+while ($product = $result->fetch_assoc()) {
+    if (isSet($products[$product['product_id']])) {
+        array_push($products[$product['product_id']]['tag_names'], $product['tag_name']);
+    } else {
+        $products[$product['product_id']] = array(
+            'product_id' => $product['product_id'],
+            'product_name' => $product['product_name'],
+            'image_path' => $product['image_path'],
+            'price' => $product['price'],
+            'stock' => $product['stock'],
+            'tag_names' => array($product['tag_name'])
+        );
+    }
+}
+echo '<script>console.log('.json_encode($products, JSON_HEX_TAG).');</script>';
+
+$_SESSION['product'] = null;
+if (isset($_POST['product_id'])) {
+    $_SESSION['product'] = $products[$_POST['product_id']];
+}
+
+$loggedin = !!isset($_SESSION['loggedin']);
+// $stmt = $con->prepare('SELECT activation_code FROM accounts WHERE id = ?');
+// $stmt->bind_param('i', $_SESSION['id']);
+// $stmt->execute();
+// $stmt->bind_result($activation_code);
+// $stmt->fetch();
+// $stmt->close();
+
+$stmt->close();
 
 ?>
 
@@ -8,45 +48,100 @@ session_start();
 <html>
     <head>
         <meta charset='utf-8'>
-        <link rel='icon' type='image/png' href='media/favicon.png' />
-        <link rel='manifest' href='manifest.json' />
+        <link rel='icon' type='image/png' href='./media/favicon.png' />
+        <link rel='manifest' href='./manifest.json' />
         <meta name='viewport' content='width=device-width, initial-scale=1'>
         
-        <title>My Account</title>
-        <link href='style/custom.css' rel='stylesheet' type='text/css'>
+        <title>Professor Hua's Store</title>
+        <link href='./style/custom.css' rel='stylesheet' type='text/css'>
     </head>
-    <body class='outer'>
-        <div class='middle'>
-            <div class='d-flex flex-column align-items-center justify-content-center flex-nowrap' style='height:100%'>
-                <!-- Judge the professor for not letting me use bootstrap -->
-                <a href="..">
-                    <img style='height:140px;' src='../media/hua_logo.png' alt='' />
-                </a>
-                <!-- <div style='width:64px;height:64px;margin-bottom:-67px;z-index:1;border-radius:100%;background-color:white;'></div>
-                <div style='width:70px;height:70px;border-radius:100%;background-color:rgb(8,81,114);'></div> -->
-                <!-- <div class='fs-14' style='padding-top:1rem;color:#8F9BB3;font-weight:bold;'>Sign In</div> -->
-                
-                <form action='calls/login.php' method='post' class='d-flex flex-column auth-form-container' style='padding:1rem 2rem;margin-top:3rem;'>
-
-                    <input class='auth-input fullwidth fs-12' type='email' name='email' placeholder='+ Email' id='email' required>
-                    <input class='auth-input fullwidth fs-12' style='margin-top:1rem;' type='password' name='password' placeholder='+ Password' id='password' required>
-                    <?php
-                        echo '<p class="fs-10 d-flex justify-content-between flex-row flex-wrap" style="color:red;margin-top:5px;">
-                            <span id="foo" style="display:none;">&#9785; '.$_SESSION['display_error'].'</span>
-                            <a href="reset" class="anchor">Forgot password?</a>
-                        </p>';
-
-                        if (isset($_SESSION['display_error']) && !empty($_SESSION['display_error'])) {
-                            echo '<script>document.getElementById("foo").style = "display:block"</script>';
-                            // echo '<script>
-                            //     window.setTimeout(() => { document.getElementById("foo").style = "display:block" }, 5000)
-                            // </script>';
-                        }
-                        // echo '<p class="fs-08" style="color:gray;text-align:right;margin-top:5px;"><a class="anchor">Forgot password?</a></p>';
-                    ?>
-
-                    <button class='auth-button fullwidth fs-14' style='margin-top:4rem;border-radius:3px;' type='submit'>Sign In</button>
-                    <p class='fs-10' style='color:gray;text-align:center;margin-top:2rem;'>Don't have an account? <a class='anchor' href='/signup'>Sign Up</a></p>
+    <body>
+        <div class='d-flex flex-column'>
+            <div class='navbar'>
+                <div class='container'>
+                    <div class='d-flex align-items-center'>
+                        <img style='height:5rem;' src='media/hua_logo.png' alt='' />
+                        <div class='navbar-links'>
+                            <div class='searchbar'>
+                                <input placeholder='fuzzy search' class='search-input' />
+                                <button class='search-button'>
+                                    <img style='height:1.5rem;' src='media/search.png' alt='' />
+                                </button>
+                            </div>
+                            <!-- <div class='profile-link'>
+                                <div class='top'>Welcome</div>
+                                <a href='login' class='bottom'>Sign In/Up</a>
+                            </div> -->
+                           
+                            <?php
+                                if ($loggedin) {
+                            ?>
+                                <div class='profile-link'>
+                                    <div class='top'>Welcome</div>
+                                    <a href='login' class='bottom'>
+                                        <?php echo ucfirst($_SESSION['username']); ?>
+                                    </a>
+                                </div>
+                            <?php
+                                } else {
+                            ?>
+                                <div class='dropdown'>
+                                    <div class='profile-link'>
+                                        <div class='top'>Welcome</div>
+                                        <div class='bottom'>Sign In/Up</div>
+                                    </div>
+                                    <div class='dropdown-content'>
+                                        <a href='./authentication/login'>Sign In</a>
+                                        <a href='./authentication/signup'>Sign Up</a>
+                                    </div>
+                                </div>
+                            <?php
+                                }
+                            ?>
+                            <a href='./checkout' style='text-decoration:none'>
+                                <div class='cart'>
+                                    <img style='height:4rem;' src='media/cart.png' alt='' />
+                                    <?php if (isset($_SESSION['product'])) { ?>
+                                        <div class='cart-display'>
+                                            <div class='top'><?php echo money_format('%n', $_SESSION['product']['price']); ?></div>
+                                            <div class='bottom'>1 item</div>
+                                        </div>
+                                    <?php } ?>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class='container'>
+                <form method='post'>
+                    <div class='card-group'>
+                        <?php
+                            foreach ($products as $product) {
+                        ?>
+                            <div class='card'>
+                                <div class='card-image'>
+                                    <img src=<?php echo '"'.$product['image_path'].'"'; ?> alt=<?php echo '"'.$product['product_name'].'"'; ?> />
+                                </div>
+                                <div class='card-body'>
+                                    <h1>
+                                        <?php echo $product['product_name']; ?>
+                                    </h1>
+                                    <div>
+                                        <div class='chip'><?php echo implode('</div><div class="chip">', $product['tag_names']); ?></div>
+                                    </div>
+                                    <div class='price'><?php echo money_format('%n', $product['price']); ?></div>
+                                </div>
+                                <div class='coupon'>
+                                    <div class='tag'>Coupon</div>
+                                    <div class='expiration'>Expires in 3 days 11 hrs</div>
+                                </div>
+                                <button type='submit' name='product_id' value=<?php echo '"'.$product['product_id'].'"'; ?>>Add to Cart</button>
+                            </div>
+                        <?php
+                            }
+                        ?>
+                    </div>
                 </form>
             </div>
         </div>
