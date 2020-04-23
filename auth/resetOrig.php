61d582bda009da775c30c50aa18bc11eaa68e567
Mei Huan Lafferty-Levdansky <meimeilafferdanski@gmail.com>
2020-04-23 13:14:08 -0500

    old password reset page

---
+++ auth/resetOrig.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <link rel='icon' type='../image/png' href='../media/favicon.png' />
+        <link rel='manifest' href='../manifest.json' />
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+        <title>Reset Password</title>
+        <link href='../style/custom.css' rel='stylesheet' type='text/css'>
+    </head>
+    <body class='outer'>
+        <div class='middle'>
+            <div class='d-flex flex-column align-items-center justify-content-center flex-nowrap' style='height:100%;'>
+                <div style='width:350px;'>
+                    <div class='d-flex flex-row justify-content-between' style='margin:.1rem .25rem;background-color:rgb(240,240,245);'>
+                        <a href='./signin.php' class='fs-15' style='text-decoration:none;color:#8F9BB3;font-weight:bold;'>So<br>sad, Alexa<br>play<br>Despacito 2.0</a>
+                        <a href='./signin.php' class='fs-15' style='text-decoration:none;color:#8F9BB3;font-weight:bold;'>So<br>sad, Alexa<br>play<br>Despacito 2.0</a>
+                    </div>
+                    <div class='d-flex flex-row justify-content-between' style='margin:.1rem .25rem;'>
+                        <a href='./signin.php' class='fs-15' style='text-decoration:none;color:#ccd5e6;font-weight:bold;'>So<br>sad, Alexa<br>play<br>Despacito 2.0</a>
+                        <a href='./signin.php' class='fs-15' style='text-decoration:none;color:#ccd5e6;font-weight:bold;'>So<br>sad, Alexa<br>play<br>Despacito 2.0</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
